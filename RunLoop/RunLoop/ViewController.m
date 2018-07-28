2951738c8ad3c5f7ca6222be039213c4f31e8508
taojigu <taojigu@163.com>
2018-07-28 19:45:01 +0800

    添加线程中通讯

--- RunLoop/RunLoop/ViewController.m
+++ RunLoop/RunLoop/ViewController.m
@@ -8,9 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <NSMachPortDelegate>
 
 @property (nonatomic, assign) NSInteger clickCount;
+@property (nonatomic, strong) NSMachPort *mainPort;
 
 @end
 
@@ -19,6 +20,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.mainPort = [[NSMachPort alloc] init];
+    self.mainPort.delegate = self;
+    [[NSRunLoop mainRunLoop] addPort:self.mainPort forMode:NSRunLoopCommonModes];
 }
 
 
@@ -31,6 +35,7 @@
     self.clickCount ++;
     NSInteger thisCount = self.clickCount;
     
+    //每次点击，生成一个新的 Thread，打印 NewThread is Running 之后，需要保证当前 Thread 仍然处于活跃状态，三秒钟后打印log
     NSThread *thread = [[NSThread alloc] initWithBlock:^{
         NSLog(@"NewThread is Running");
         [self performSelector:@selector(printLog:) withObject:@(thisCount) afterDelay:3];
@@ -47,6 +52,45 @@
 - (IBAction)stopRunLoopClicked:(id)sender {
     
 }
+
+# pragma mark -- 线程间通讯
+
+- (IBAction)threadCommunicateButtonClicked:(id)sender {
+    dispatch_queue_t queue = dispatch_queue_create("com.RunLoop", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_async(queue, ^{
+        @autoreleasepool{
+            [self sendSubThreadMessage];
+        }
+    });
+}
+
+- (void)sendSubThreadMessage{
+    if ([NSThread currentThread] == [NSThread mainThread]) {
+        return;
+    }
+    NSLog(@"Start at sub thread");
+    [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:10]];
+    
+    NSMachPort *thisPort = [[NSMachPort alloc] init];
+    NSString *componetString = @"ContextName";
+    NSData *data = [componetString dataUsingEncoding:NSUTF8StringEncoding];
+    NSMutableArray *components = [[NSMutableArray alloc] initWithArray:@[data]];
+    [self.mainPort sendBeforeDate:[NSDate date] components:components from:thisPort reserved:0];
+    
+}
+
+#pragma mark --
+
+- (void)handleMachMessage:(void *)msg {
+    if ([NSThread currentThread] != [NSThread mainThread]){
+        NSLog(@"Not handle mach message outside main thread");
+    }
+    //NSMessagePort *msgPort = (__bridge NSMessagePort *)msg;
+    //NSArray *components = [msgPort valueForKey:@"components"];
+    //NSLog(@"Component from sub thread: %@",components);
+}
+
+
 
 
 @end
